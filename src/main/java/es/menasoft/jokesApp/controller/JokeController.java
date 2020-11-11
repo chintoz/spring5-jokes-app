93d658f9d4f230dc7defde7f5c75a441d43d349f
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2020-11-11 23:52:31 +0100

    Changes to add more mapping values at controller layers. Furthermore, a banner was added to the startup process.

--- src/main/java/es/menasoft/jokesApp/controller/JokeController.java
+++ src/main/java/es/menasoft/jokesApp/controller/JokeController.java
@@ -14,8 +14,8 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping("/")
-    public String getJoke(Model model) {
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
         model.addAttribute("joke", jokeService.getJoke());
         return "chucknorris";
     }
